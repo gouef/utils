0f31dc40b8196b4bf157325dc6206634523cb36c
Jan Galek <admin@gcore.cz>
2024-11-29 22:43:46 +0100

    Added substr

---
+++ strings.go
@@ -0,0 +1,22 @@
+package utils
+
+func Substr(s string, start int, length *int) string {
+	if start < 0 {
+		start = len(s) + start
+	}
+	if start < 0 || start >= len(s) {
+		return ""
+	}
+
+	var end int
+	if length == nil {
+		end = len(s)
+	} else {
+		end = start + *length
+	}
+
+	if end > len(s) {
+		end = len(s)
+	}
+	return s[start:end]
+}
