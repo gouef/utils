caeec5b57187f9d48119f1e7934780daa87ce061
Jan Galek <admin@gcore.cz>
2024-11-29 16:57:51 +0100

    Added explode, implode functions

--- array.go
+++ array.go
@@ -1,5 +1,7 @@
 package utils
 
+import "strings"
+
 func InArray[T comparable](needle T, haystack []T) bool {
 	for _, item := range haystack {
 		if item == needle {
@@ -7,4 +9,12 @@ func InArray[T comparable](needle T, haystack []T) bool {
 		}
 	}
 	return false
+}
+
+func Explode(separator string, stringStr string) []string {
+	return strings.Split(stringStr, separator)
+}
+
+func Implode(separator string, elements []string) string {
+	return strings.Join(elements, separator)
 }
