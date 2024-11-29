a442ad177de6b7a42295988627e10db5383aa8f9
Jan Galek <admin@gcore.cz>
2024-11-29 23:06:24 +0100

    Added stringCompare

--- strings.go
+++ strings.go
@@ -23,6 +23,32 @@ func Substr(s string, start int, length *int) string {
 	return s[start:end]
 }
 
+func Substring(s string, start int, length *int) string {
+	return Substr(s, start, length)
+}
+
 func Strpos(stringStr string, needle string) int {
 	return strings.LastIndex(stringStr, needle)
+}
+
+func StringPosition(stringStr string, needle string) int {
+	return Strpos(stringStr, needle)
+}
+
+func Strncmp(s1, s2 string, n int) int {
+	sub1 := s1
+	sub2 := s2
+
+	if len(s1) > n {
+		sub1 = s1[:n]
+	}
+	if len(s2) > n {
+		sub2 = s2[:n]
+	}
+
+	return strings.Compare(sub1, sub2)
+}
+
+func StringCompare(s1, s2 string, n int) int {
+	return Strncmp(s1, s2, n)
 }
